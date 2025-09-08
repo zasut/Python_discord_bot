cc054397bfc9f8b3b2899943be14b7e57b559c32
jaden <66454947+zasut@users.noreply.github.com>
2025-09-08 13:08:51 -0700

    Implements Hello & Repeat command

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import discord 
 import os
 from dotenv import load_dotenv
+from discord import app_commands
 
 load_dotenv()
 
 API_KEY = os.getenv("KEY")
 
-intents = discord.Intents.default()
+MY_GUILD = discord.Object(id=1407756705068224622)
 
-client = discord.Client(intents=intents)
+
+class MyClient(discord.Client):
+    user: discord.ClientUser
+
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+@client.tree.command()
+@app_commands.describe(
+    message='The message to repeat',
+)
+async def repeat(interactions: discord.Interaction, message: str):
+    """"repeats a message."""
+    await interactions.response.send_message(message)
 
 
 
